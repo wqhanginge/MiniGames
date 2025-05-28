5d8b40b44d25c3281c96c77fb2571617c1d08ef3
Gee Wang <54838305+wqhanginge@users.noreply.github.com>
2025-05-28 22:28:34 +0800

    [Laptop]Update sudoku

--- Sudoku/sudoku.cpp
+++ Sudoku/sudoku.cpp
@@ -11,40 +11,53 @@
 
 constexpr size_t SEG = 3, ROW = 9, MAP = 81;
 constexpr std::string_view DIGITS = "0123456789";
+constexpr size_t DSIZE = DIGITS.size();
 constexpr char BLANK = DIGITS[0];
 
 
 class SudokuMap :public std::array<char, MAP> {
     using _Base = std::array<char, MAP>;
-    constexpr size_t _doff(size_t offset) const { return static_cast<size_t>(_Base::at(offset) - BLANK); }
+    size_t _doff(size_t offset) const { return static_cast<size_t>(_Base::at(offset) - BLANK); }
+    size_t _next(size_t offset) const { while (offset < MAP && _doff(offset)) offset++; return offset; }
+
+    bool _solve(size_t offset) {
+        if (offset >= MAP) return true;
+        for (auto digit : DIGITS.substr(1)) {
+            _Base::at(offset) = digit;
+            if (unique(offset) && _solve(_next(offset))) return true;
+        }
+        _Base::at(offset) = BLANK;
+        return false;
+    }
 
 public:
     SudokuMap() { _Base::fill(BLANK); }
-    SudokuMap(const std::string_view map) : SudokuMap() {
+    explicit SudokuMap(std::string_view map) :SudokuMap() {
         for (size_t i = 0; i < std::min(MAP, map.size()); i++) {
-            _Base::at(i) = DIGITS[std::min(static_cast<size_t>(map[i] - BLANK), DIGITS.size()) % DIGITS.size()];
+            _Base::at(i) = DIGITS[std::min(static_cast<size_t>(map[i] - BLANK), DSIZE) % DSIZE];
         }
     }
 
     constexpr explicit operator std::string_view() const { return std::string_view(_Base::data(), MAP); }
-    constexpr bool unique(size_t offset) const {
-        const size_t x = offset / ROW, y = offset % ROW, bx = x / SEG * SEG, by = y / SEG * SEG;
-        const size_t stride = DIGITS.size();
-        std::array<bool, stride * 3> flags = {};
+    bool unique(const size_t offset) const {
+        size_t x = offset / ROW, y = offset % ROW, b = ((x / SEG) * ROW + (y / SEG)) * SEG;
+        bool duplicate = false, flags[3][DSIZE] = {};
         for (size_t i = 0; i < ROW; i++) {
-            size_t irow = 0*stride+_doff(x * ROW + i), icol = _doff(i * ROW + y), iblk = _doff((bx + i / SEG) * ROW + (by + i % SEG));
-            if (flags[irow] || flags[icol] || flags[iblk]) return false;
-            flags[irow] |= static_cast<bool>(irow);
-            flags[icol] |= static_cast<bool>(icol);
-            flags[iblk] |= static_cast<bool>(iblk);
+            size_t ix = _doff(x * ROW + i), iy = _doff(i * ROW + y), ib = _doff(b + (i / SEG) * ROW + (i % SEG));
+            duplicate |= flags[0][ix] || flags[1][iy] || flags[2][ib];
+            flags[0][ix] |= static_cast<bool>(ix);
+            flags[1][iy] |= static_cast<bool>(iy);
+            flags[2][ib] |= static_cast<bool>(ib);
         }
-        return true;
+        return !duplicate;
     }
+
+    SudokuMap& solve() { _solve(_next(0)); return *this; }
 };
 
 
 int main() {
     SudokuMap smap("007008000095002600000100250010000079600009000008004000000000000006400800030050020");
-    std::vector<SudokuMap> sols;
+    std::cout << std::string_view(smap) << '\n' << std::string_view(smap.solve());
     return 0;
 }
